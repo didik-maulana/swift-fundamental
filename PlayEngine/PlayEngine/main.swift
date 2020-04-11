cbbf712b662fafad4d1d4ac69bc9e09ac81e1835
Didik Maulana Ardiansyah <didik@mamiteam.com>
2020-04-12 00:09:28 +0700

    Completed codelab Module 07 Closure

--- PlayEngine/PlayEngine/main.swift
+++ PlayEngine/PlayEngine/main.swift
@@ -8,5 +8,87 @@
 
 import Foundation
 
-print("Hello, World!")
+func getInput(message: String) -> String? {
+    print(message, terminator: " : ")
+    return readLine()
+}
 
+func getInputInt(message: String) -> Int? {
+    if let inputString = getInput(message: message) {
+        return Int(inputString)
+    } else {
+        return nil
+    }
+}
+
+var speedTotal = 0
+
+func increaseSpeed(forIncrease amount: Int) -> () -> Int {
+    func incremental() -> Int {
+        speedTotal += amount
+        return speedTotal
+    }
+    return incremental
+}
+
+func decreaseSpeed(forDescrease amount: Int) -> () -> Int {
+    func decremental() -> Int {
+        speedTotal -= amount
+        return speedTotal
+    }
+    return decremental
+}
+
+let incrementByTen = increaseSpeed(forIncrease: 10)
+let decrementByTen = decreaseSpeed(forDescrease: 10)
+
+func startEngine() {
+    print("Starting engine on..")
+    print("Brem brem brem")
+}
+
+func stopEngine() {
+    print("Turn off engine.")
+}
+
+func showSpeed() {
+    print("Current speed is \(speedTotal)")
+}
+
+func action() {
+    print("----------------------------------------")
+    print("Choose menu :")
+    print("[1] Increase speed")
+    print("[2] Decrease speed")
+    print("[3] Turn off engine")
+    
+    if let state = getInputInt(message: "Input menu") {
+        switch state {
+        case 1:
+            speedTotal = incrementByTen()
+            showSpeed()
+            action()
+        case 2:
+            if speedTotal > 0 {
+                speedTotal = decrementByTen()
+                showSpeed()
+                action()
+            } else {
+                stopEngine()
+                break
+            }
+        case 3:
+            stopEngine()
+            break
+        default:
+            print("Input is not valid")
+            action()
+            break
+        }
+    } else {
+        print("Input is not valid")
+    }
+}
+
+startEngine()
+action()
